30c05502eeb0c3b8f55ad374adca51c29625e3aa
Nikita-Burlakov <94919560+Nikita-Burlakov@users.noreply.github.com>
2022-05-08 23:52:18 +0300

    Add files via upload
    "Список профессий"

--- flask_learn_2.py
+++ flask_learn_2.py
@@ -3,6 +3,24 @@ from flask import Flask, url_for, request, render_template
 app = Flask(__name__)
 
 
+prof_list = ["инженер-исследователь",
+             "пилот",
+             "строитель",
+             "экзобиолог",
+             "врач",
+             "инженер по терраформированию",
+             "климатолог",
+             "специалист по радиационной защите",
+             "астрогеолог",
+             "гляциолог",
+             "инженер жизнеобеспечения",
+             "метеоролог",
+             "оператор марсохода",
+             "киберинженер",
+             "штурман",
+             "пилот дронов"]
+
+
 @app.route('/index/')
 def test_render():
     param = dict()
@@ -19,6 +37,18 @@ def base_render():
 @app.route('/child_page/<title>')
 def child_render(title):
     return render_template("index.html", title=title)
+
+
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    params = {"prof_list": prof_list,
+              "href": url_for('static', filename='css/style.css')}
+
+    if list_type != "ol" and list_type != "ul":
+        return "Неправильный формат списка"
+    elif list_type == "ul":
+        return render_template("index_2.html", ul_list=True, **params)
+    return render_template("index_2.html", ul_list=False, **params)
 
 
 if __name__ == '__main__':
